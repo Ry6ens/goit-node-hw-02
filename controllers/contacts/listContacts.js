558c63a86c07f0f40a28c2339f60dbc3d39706f5
Sergey Gulaga <ry6ens@gmail.com>
2022-11-04 09:03:05 +0200

    update app

--- controllers/contacts/listContacts.js
+++ controllers/contacts/listContacts.js
@@ -1,5 +1,4 @@
-// const contacts = require("../../models/contacts.js");
-const {Contact} = require("../../models/contact")
+const { Contact } = require("../../models/contact");
 
 const listContacts = async (req, res) => {
   const result = await Contact.find({}, "name email");
