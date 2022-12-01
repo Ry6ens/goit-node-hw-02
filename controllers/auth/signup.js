b0b6401f11e31461c777ee9623da8bcce390fa29
Sergey Gulaga <ry6ens@gmail.com>
2022-12-01 10:16:03 +0200

    add new task

--- controllers/auth/signup.js
+++ controllers/auth/signup.js
@@ -1,4 +1,5 @@
 const bcrypt = require("bcryptjs");
+const gravatar = require("gravatar");
 
 const { User } = require("../../models/user");
 
@@ -14,7 +15,14 @@ const signup = async (req, res) => {
 
   const hashPassword = await bcrypt.hash(password, 10);
 
-  const newUser = await User.create({ name, email, password: hashPassword });
+  const avatarURL = gravatar.url(email);
+
+  const newUser = await User.create({
+    name,
+    email,
+    password: hashPassword,
+    avatarURL,
+  });
 
   res.status(201).json({
     name: newUser.name,
